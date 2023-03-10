f7701a1e267c9539ebe9499ac9f24b17b4da7316
Mihir Sharma <mihir.sharma@stackroute.in>
2023-03-10 12:33:05 +0530

    add boilerplate

---
+++ data-analytics-service/src/test/java/com/stackroute/dataanalyticsservice/DataAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.dataanalyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
