3968eda8ea0e9d3d1623bd29f84e76a5d2103582
Mihir Sharma <mihir.sharma@stackroute.in>
2023-03-16 11:16:28 +0530

    add streaming service

---
+++ data-streaming-service/src/main/java/com/stackroute/datastreamingservice/DataStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.datastreamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStreamingServiceApplication.class, args);
+	}
+
+}
---
+++ data-streaming-service/src/test/java/com/stackroute/datastreamingservice/DataStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.datastreamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
