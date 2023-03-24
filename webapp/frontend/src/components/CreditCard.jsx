e7287521316318abaa2931bdb539d4ce32582104
Adith Pozhekadavil Anandhan <adith.panandhan@batonsystems.com>
2023-03-24 15:27:57 +0530

    Merged frontend into analytics-service-with-kafka

---
+++ webapp/frontend/src/components/CreditCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Chart1 from "./chart1";
+const CreditCard = () =>{
+    return (
+        <>
+
+<Chart1 url="country_vs_credit" name="Average Credit Score" yComponentName="averageCreditScore" title="Country vs Average Credit Score"/>
+
+        
+        </>
+    );
+};
+
+export default CreditCard;
---
+++ webapp/frontend/src/components/Salary.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Chart1 from "./chart1";
+
+const Salary = () =>{
+    return (
+        <>
+          <Chart1 url="country_vs_salary" name="Average Salary" yComponentName="averageSalary" title="Country vs  Average Salary"/>
+
+        
+        </>
+    );
+};
+
+export default Salary;
