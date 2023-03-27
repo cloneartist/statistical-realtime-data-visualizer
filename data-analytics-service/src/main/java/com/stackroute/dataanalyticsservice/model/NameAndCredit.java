71acb7fe098bbf529a05b65502ce8515df9df320
Adith Pozhekadavil Anandhan <adith.panandhan@batonsystems.com>
2023-03-27 15:17:56 +0530

    correct mismatch error

--- data-analytics-service/src/main/java/com/stackroute/dataanalyticsservice/model/NameAndCredit.java
+++ data-analytics-service/src/main/java/com/stackroute/dataanalyticsservice/model/NameAndCredit.java
@@ -2,40 +2,33 @@ package com.stackroute.dataanalyticsservice.model;
 
 public class NameAndCredit {
 	private String name;
-	private float averageCreditScore;
-	
-	
-	
-	
+	private double averageCreditScore;
+
 	public NameAndCredit() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
-	
-	public NameAndCredit(String name, float averageCreditScore) {
+
+	public NameAndCredit(String name, double averageCreditScore) {
 		super();
 		this.name = name;
 		this.averageCreditScore = averageCreditScore;
 	}
 
-
-
-
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public float getAverageCreditScore() {
+
+	public double getAverageCreditScore() {
 		return averageCreditScore;
 	}
-	public void setAverageCreditScore(float averageCreditScore) {
+
+	public void setAverageCreditScore(double averageCreditScore) {
 		this.averageCreditScore = averageCreditScore;
 	}
-	
-	
+
 }
