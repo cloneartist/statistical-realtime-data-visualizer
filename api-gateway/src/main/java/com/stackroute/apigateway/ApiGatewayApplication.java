bf41fae48ccf37221f647e6ccc240e087669640a
Daivik <daivik.kesav@batonsystems.com>
2023-03-27 18:12:37 +0530

    API Gateway

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,23 @@ package com.stackroute.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	/*
+	 * @Bean public RouteLocator gateWayRoutes(RouteLocatorBuilder builder ) {
+	 *
+	 * }
+	 */
+
 
 }
